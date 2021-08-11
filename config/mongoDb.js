eca2706fd817536cfcf2aa2f4de2dec0dff10ac1
amanKumar689 <70095044+amanKumar689@users.noreply.github.com>
2021-08-11 14:07:58 +0530

    Delete directory

--- config/mongoDb.js
+++
@@ -1,3 +0,0 @@
-const MongoDb_connectionString =
-  "mongodb+srv://AmanZone:lPKHPV14EjoBGMjY@cluster0.kknzw.mongodb.net/social_graphql_react?retryWrites=true&w=majority";
-module.exports = { MongoDb_connectionString };
--- config/secretKey.js
+++
@@ -1,1 +0,0 @@
-module.exports.secretKey = 'SOME SPECIAL POWER'
